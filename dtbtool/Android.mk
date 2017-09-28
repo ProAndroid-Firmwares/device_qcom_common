8e1acf19dc780111d4fb93186b1a916b5d5d312b
AudioGod <audiogod.adi@gmail.com>
2017-09-28 11:08:47 +0530

    DTBTool: Switch to QCOM's Latest DTB Tool
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- dtbtool/Android.mk
+++ dtbtool/Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(BOARD_KERNEL_SEPARATED_DT),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -9,9 +8,7 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS += \
 	-Wall
 
-## Hybrid v1/v2 dtbTool. Use a different name to avoid conflicts with copies in device repos
-LOCAL_MODULE := dtbToolCM
+LOCAL_MODULE := dtbTool
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_EXECUTABLE)
-endif
